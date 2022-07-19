27758bd480aee8310bccf7697b3df3b8c991bcf7
BrickBrigade <101427661+BrickBrigade@users.noreply.github.com>
2022-07-19 00:39:09 -0500

    init

--- src/client/js/makeLocationList.js
+++ src/client/js/makeLocationList.js
@@ -1,14 +1,10 @@
 const handleSelect = require("./handleSelect");
 
-function printList() {
+export default function printList() {
 	console.log("printList() called");
 	let country = document.getElementById("country").value;
 	let name = document.getElementById('city').value;
 	let state = document.getElementById('state').value;
-
-
-
-	// let adminCode1 = document.getElementById('state').value;
 
 	let params = new URLSearchParams({ paramstring: `name=${name}&country=${country}&adminCode1=${state}` });
 
@@ -40,29 +36,8 @@ function printList() {
 				item.dataset.coords = [res.geonames[i].lat, res.geonames[i].lng];
 				nameList.appendChild(item);
 			}
-
-
 		document.appendChild(fragment);
-		// let nameresult = document.getElementById('namelist');
-		// if (nameresult.firstChild!=null)
-		// 	while (nameresult.firstChild) {
-		// 		nameresult.removeChild(nameresult.firstChild);
-		// 		console.log('removed child');
-		// 	}
-		// let infoDiv = document.getElementById('locationInfoContainer');
-		// if (infoDiv != null) {
-		// 	while (infoDiv.firstChild){
-		// 		infoDiv.removeChild(infoDiv.firstChild);
-		// 	}
-		// }
-		// for (let i = 0; i < res.geonames.length; i++) {
-		// 	let li = document.createElement('li');
-		// 	nameresult.appendChild(li);
-		// 	li.innerHTML= res.geonames[i].name+', '+res.geonames[i].adminName1;
-		// 	li.className = 'searchName';
-		// 	li.dataset.index = i;
-		// 	li.dataset.coords = [res.geonames[i].lat, res.geonames[i].lng];
-		// }
+
 		handleSelect(res);
 	});
 }
